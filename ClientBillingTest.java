c8dd5491878a10859c1d1392cf1b60ec477d29c1
Paulo Motta <prmottajr@gmail.com>
2018-06-24 01:12:25 -0300

    Added two tests for special situations
    Added a test for when the query date is the same as the initial date
    Added a test for when the user passes in an invalid date, uses the lenient property of the Calendar to return a valid billing

--- ClientBillingTest.java
+++ ClientBillingTest.java
@@ -314,4 +314,44 @@ public class ClientBillingTest {
         assertEquals(expResult.getStart(), result.getStart());
         assertEquals(expResult.getEnd(), result.getEnd());
     }
+    
+    /**
+     * Given a date 
+     * When it is the same as the initial
+     * Then return a billing cycle
+     */
+    @Test
+    public void testBillingCycleSameDay() throws ParseException {
+        System.out.println("testBillingCycleSameDay");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date queryDate = sdf.parse("15-02-2018");
+        Date initial = sdf.parse("15-02-2018");
+        Date start = sdf.parse("15-02-2018");
+        Date end = sdf.parse("17-03-2018");
+        ClientBilling instance = new ClientBilling(initial);
+        BillingCycle expResult = new BillingCycle(start, end);
+        BillingCycle result = instance.getBillingCycle(queryDate);
+        assertEquals(expResult.getStart(), result.getStart());
+        assertEquals(expResult.getEnd(), result.getEnd());
+    }
+    
+    /**
+     * Given a date 
+     * When it is an invalid date
+     * Then return a billing cycle based on the lenient property of the Calendar
+     */
+    @Test
+    public void testBillingCycleInvalidQryDay() throws ParseException {
+        System.out.println("testBillingCycleInvalidQryDay");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date queryDate = sdf.parse("30-02-2018");
+        Date initial = sdf.parse("15-02-2018");
+        Date start = sdf.parse("15-02-2018");
+        Date end = sdf.parse("17-03-2018");
+        ClientBilling instance = new ClientBilling(initial);
+        BillingCycle expResult = new BillingCycle(start, end);
+        BillingCycle result = instance.getBillingCycle(queryDate);
+        assertEquals(expResult.getStart(), result.getStart());
+        assertEquals(expResult.getEnd(), result.getEnd());
+    }
 }
