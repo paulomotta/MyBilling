0b7635713ca087ee6af6dfd245d7e6b8acc3040b
Paulo Motta <prmottajr@gmail.com>
2018-06-23 23:24:58 -0300

    Added a test case, corrected the name of others
    Added a test case for ten months difference within the same year
    Corrected the name of the other test cases

--- ClientBillingTest.java
+++ ClientBillingTest.java
@@ -71,7 +71,7 @@ public class ClientBillingTest {
      */
     @Test
     public void testBillingCycleAfterFiveMonths() throws ParseException {
-        System.out.println("testBillingCycleAfterThreeMonths");
+        System.out.println("testBillingCycleAfterFiveMonths");
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         Date queryDate = sdf.parse("05-08-2018");
         Date initial = sdf.parse("15-03-2018");
@@ -100,6 +100,26 @@ public class ClientBillingTest {
         BillingCycle expResult = null;
         BillingCycle result = instance.getBillingCycle(queryDate);
         assertEquals(expResult, result);
+    }
+    
+    /**
+     * Given a date 
+     * When it is after ten months within the same year
+     * Then return a billing cycle
+     */
+    @Test
+    public void testBillingCycleAfterTenMonths() throws ParseException {
+        System.out.println("testBillingCycleAfterTenMonths");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Date queryDate = sdf.parse("05-11-2018");
+        Date initial = sdf.parse("15-02-2018");
+        Date start = sdf.parse("15-10-2018");
+        Date end = sdf.parse("14-11-2018");
+        ClientBilling instance = new ClientBilling(initial);
+        BillingCycle expResult = new BillingCycle(start, end);
+        BillingCycle result = instance.getBillingCycle(queryDate);
+        assertEquals(expResult.getStart(), result.getStart());
+        assertEquals(expResult.getEnd(), result.getEnd());
     }
     
 }
